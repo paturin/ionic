dee6b69e2fd24bd7a013be54195a8d435c8973f2
Valentin Seguin <valentinseguin@gmail.com>
2024-01-24 00:56:13 +0100

    Saving Photos to the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,51 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Preferences } from '@capacitor/preferences';
+//import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root'
 })
 
-
 export class PhotoService {
   public photos: UserPhoto[] = [];
-  
 
-  constructor() { }
-
-
-
+  constructor(
+  ) { }
 
   public async addNewToGallery() {
-
-    
-
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     });
-
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
-
     this.photos.unshift(savedImageFile);
-    
+    console.log("this.photos > ", this.photos);
     // this.photos.unshift({
     //   filepath: "soon...",
     //   webviewPath: capturedPhoto.webPath!
     // });
-
-    
-    
   }
 
   private async savePicture(photo: Photo) {
     // Convert photo to base64 format, required by Filesystem API to save
     const base64Data = await this.readAsBase64(photo);
-  
     // Write the file to the data directory
     const fileName = Date.now() + '.jpeg';
     const savedFile = await Filesystem.writeFile({
@@ -53,7 +40,8 @@ export class PhotoService {
       data: base64Data,
       directory: Directory.Data
     });
-  
+    console.log("savedFile > ", savedFile);
+
     // Use webPath to display the new image instead of base64 since it's
     // already loaded into memory
     return {
@@ -64,12 +52,12 @@ export class PhotoService {
 
   private async readAsBase64(photo: Photo) {
     // Fetch the photo, read as a blob, then convert to base64 format
+    // A BLOB (binary large object) is a varying-length binary string that can be up to 2,147,483,647 characters long.
     const response = await fetch(photo.webPath!);
     const blob = await response.blob();
-  
     return await this.convertBlobToBase64(blob) as string;
   }
-  
+
   private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onerror = reject;
